59dca62f704228b45e60fcabfdbccc239e10da8c
Adam Bergman <adam@fransvilhelm.com>
2018-06-04 10:57:37 +0200

    chore: Add custom styleguide example wrapper

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,7 +1,12 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require('path');
 const { createConfig } = require('@webpack-blocks/webpack');
 const babel = require('@webpack-blocks/babel');
 
 module.exports = {
   components: 'src/**/[A-Z]*.js',
   webpackConfig: createConfig([babel()]),
+  styleguideComponents: {
+    Wrapper: path.join(__dirname, 'styleguidist/Wrapper'),
+  },
 };
---
+++ styleguidist/Wrapper.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  padding: 1em;
+  background: #f1f1f1;
+  color: #444;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+    Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif;
+  font-size: 13px;
+  line-height: 1.4em;
+  min-width: 600px;
+`;
+
+export default Wrapper;
