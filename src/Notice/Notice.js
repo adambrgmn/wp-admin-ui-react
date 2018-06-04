c48cf0ba55f0d54763ec96508741cd75c9a6d9f7
Adam Bergman <adam@fransvilhelm.com>
2018-06-04 15:48:13 +0200

    feat: Add Notice component

---
+++ src/Notice/Notice.js
@@ -0,0 +1,117 @@
+import { createElement } from 'react';
+import styled, { css } from 'styled-components';
+import PropTypes from 'prop-types';
+import {
+  applyStyleModifiers,
+  styleModifierPropTypes,
+} from 'styled-components-modifiers';
+
+const MODIFIER_CONFIG = {
+  info: () => `
+    border-left-color: #00a0d2;
+  `,
+  success: () => `
+    border-left-color: #46b450;
+  `,
+  warning: () => `
+    border-left-color: #ffb900;
+  `,
+  error: () => `
+    border-left-color: #dc3232;
+  `,
+};
+
+const NoticeDismiss = styled.button`
+  position: absolute;
+  top: 50%;
+  right: 1px;
+  margin: 0;
+  border: none;
+  padding: 9px;
+  background: none;
+  color: #72777c;
+  cursor: pointer;
+  transform: translateY(-50%);
+
+  &:hover::before,
+  &:active::before,
+  &:focus::before {
+    color: #cc0000;
+  }
+
+  &:focus {
+    outline: none;
+    box-shadow: 0 0 0 1px #5b9dd9, 0 0 2px 1px rgba(30, 140, 190, 0.8);
+  }
+
+  &::before {
+    content: '\f153';
+    display: block;
+    height: 20px;
+    width: 20px;
+    background: none;
+    color: #72777c;
+    font-family: dashicons;
+    font-size: 16px;
+    font-weight: normal;
+    line-height: 20px;
+    text-align: center;
+    speak: none;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
+
+const NoticeWrapper = styled.div`
+  margin: 5px 15px 2px;
+  border-left: 4px solid #fff;
+  padding: 10px 20px;
+  background: #fff;
+  box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.1);
+  transform: translateY(0);
+  opacity: 1;
+  visibility: visible;
+  transition: transform 0.5s ease-in-out, opacity 0.5s ease-in-out,
+    visibility 0.5s ease-in-out;
+
+  ${props =>
+    props.dismissable &&
+    css`
+      padding-right: 38px;
+      position: relative;
+    `};
+
+  ${props =>
+    props.hide &&
+    css`
+      transform: translateY(-100%);
+      opacity: 0;
+      visibility: hidden;
+    `};
+
+  ${applyStyleModifiers(MODIFIER_CONFIG)};
+`;
+
+function Notice(props) {
+  return createElement(
+    NoticeWrapper,
+    props,
+    props.children,
+    props.dismissable
+      ? createElement(NoticeDismiss, { onClick: props.onClick })
+      : null,
+  );
+}
+
+Notice.propTypes = {
+  dismissable: PropTypes.arrayOf,
+  modifiers: styleModifierPropTypes(MODIFIER_CONFIG),
+};
+
+Notice.defaultProps = {
+  dismissable: true,
+  modifiers: ['info'],
+};
+
+/** @component */
+export default Notice;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 export { Button } from './Button';
 export { Dashicon } from './Dashicon';
 export { Link } from './Link';
+export { Notice } from './Notice';
 export { Paragraph } from './Paragraph';
-export { Table } from './Table';
 export { ScreenReaderText } from './ScreenReaderText';
+export { Table } from './Table';
