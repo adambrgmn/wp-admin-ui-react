8fec0fb3cb9180cb8f47690020cf404e4c2fe4f5
Adam Bergman <adam@fransvilhelm.com>
2018-06-04 22:14:52 +0200

    chore: Migrate docs to storybook

---
+++ src/Table/Table.stories.js
@@ -0,0 +1,64 @@
+/* eslint-disable react/no-array-index-key */
+import React, { Fragment } from 'react';
+import { storiesOf } from '@storybook/react';
+import Table from './Table';
+import { Link } from '../Link';
+import { Button } from '../Button';
+
+const DefaultTableContent = () => {
+  const HeadRow = () => (
+    <tr>
+      <th>Title</th>
+      <th>Date</th>
+      <th>Remove</th>
+    </tr>
+  );
+
+  return (
+    <Fragment>
+      <thead>
+        <HeadRow />
+      </thead>
+
+      <tbody>
+        {Array.from({ length: 10 }).map((_, i) => (
+          <tr key={i}>
+            <th>
+              <Link href="/">Hello world {i}</Link>
+            </th>
+            <td>2018-01-0{i}</td>
+            <td>
+              <Button dashicon="trash" />
+            </td>
+          </tr>
+        ))}
+      </tbody>
+
+      <tfoot>
+        <HeadRow />
+      </tfoot>
+    </Fragment>
+  );
+};
+
+storiesOf('Table', module)
+  .add('standard', () => (
+    <Table>
+      <DefaultTableContent />
+    </Table>
+  ))
+  .add('striped', () => (
+    <Table striped>
+      <DefaultTableContent />
+    </Table>
+  ))
+  .add('fixed', () => (
+    <Table fixed striped>
+      <DefaultTableContent />
+    </Table>
+  ))
+  .add('hover', () => (
+    <Table striped hover>
+      <DefaultTableContent />
+    </Table>
+  ));
