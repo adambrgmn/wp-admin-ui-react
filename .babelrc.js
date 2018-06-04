3ed40160c0b1e741990977786c74a3bfa94e2b2c
Adam Bergman <adam@fransvilhelm.com>
2018-06-04 11:11:21 +0200

    chore: Install styled-components babel plugin

--- .babelrc.js
+++ .babelrc.js
@@ -1,5 +1,6 @@
 const { BABEL_ENV, NODE_ENV } = process.env;
-const cjs = BABEL_ENV === 'cjs' || NODE_ENV === 'test';
+const isProduction = NODE_ENV === 'production';
+const isCjs = BABEL_ENV === 'cjs' || NODE_ENV === 'test';
 
 module.exports = {
   presets: [
@@ -22,8 +23,15 @@ module.exports = {
   plugins: [
     ['@babel/plugin-proposal-class-properties', { loose: true }],
     ['@babel/plugin-proposal-object-rest-spread', { useBuiltIns: true }],
+    [
+      'babel-plugin-styled-components',
+      {
+        displayName: !isProduction,
+        minify: isProduction,
+      },
+    ],
     '@babel/plugin-transform-classes',
     '@babel/plugin-proposal-export-default-from',
-    cjs && '@babel/plugin-transform-modules-commonjs',
+    isCjs && '@babel/plugin-transform-modules-commonjs',
   ].filter(Boolean),
 };
